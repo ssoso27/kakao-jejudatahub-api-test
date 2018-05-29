d6379147f1bf5d0ee3281d916c2affab6374c8ad
LawrenceYap <sohi8290@gmail.com>
2018-05-29 17:42:50 +0900

    공유하기

--- test.js
+++ test.js
@@ -29,6 +29,39 @@ function KakaoApi(appKey) {
 
 	function share(keywords) { // 공유하기
 		alert(JSON.stringify(keywords));
+			Kakao.Link.sendDefault({
+			  objectType: 'feed',
+			  content: {
+				title: '딸기 치즈 케익',
+				description: '#케익 #딸기 #삼평동 #카페 #분위기 #소개팅',
+				imageUrl: 'http://mud-kage.kakao.co.kr/dn/Q2iNx/btqgeRgV54P/VLdBs9cvyn8BJXB3o7N8UK/kakaolink40_original.png',
+				link: {
+				  mobileWebUrl: 'https://developers.kakao.com',
+				  webUrl: 'https://developers.kakao.com'
+				}
+			  },
+			  social: {
+				likeCount: 286,
+				commentCount: 45,
+				sharedCount: 845
+			  },
+			  buttons: [
+				{
+				  title: '웹으로 보기',
+				  link: {
+					mobileWebUrl: 'https://developers.kakao.com',
+					webUrl: 'https://developers.kakao.com'
+				  }
+				},
+				{
+				  title: '앱으로 보기',
+				  link: {
+					mobileWebUrl: 'https://developers.kakao.com',
+					webUrl: 'https://developers.kakao.com'
+				  }
+				}
+			  ]
+			});
 	}
 
 	return { // 함수 리턴
